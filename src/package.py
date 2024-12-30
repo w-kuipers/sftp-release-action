40f29eb4abe6f0a156407a56043790ff0c0aa7ca
Wibo Kuipers <wibo@dtchdigitals.com>
2024-12-30 13:58:53 +0100

    Handle user config

--- src/package.py
+++ src/package.py
@@ -4,6 +4,7 @@
 
 import os
 import subprocess
+import json
 from src.utils import copydir, copyfile
 
 def package(version: str):
@@ -14,6 +15,12 @@ def package(version: str):
     ## Build the NODE source
     subprocess.Popen(["npm", "install"]).wait()
     subprocess.Popen(["npm", "run", "build"]).wait()
+
+    ## Get user defined included files/dirs
+    user_config = {}
+    if os.path.isfile("publish.json"):
+        with open("publish.json") as f:
+            user_config = json.load(f)
 
     basedir = "./"
     dirs = [
@@ -26,18 +33,28 @@ def package(version: str):
         "templates"
     ]
 
+    ## Add user defined indluded dirs
+    if "includeDirs" in user_config and isinstance(user_config["includeDirs"], list):
+        dirs = list(set(dirs + user_config["includeDirs"]))
+
     ## All PHP files
     files = ["style.css", "screenshot.png"]
+
+    ## Add user defined indluded files
+    if "includeFiles" in user_config and isinstance(user_config["includeFiles"], list):
+        files = list(set(files + user_config["includeFiles"]))
+
     for f in os.listdir(basedir):
         if f.endswith(".php"):
-            files.append(os.path.join(basedir, f))
+            path = os.path.join(basedir, f)
+            if not path in files:
+                files.append(path)
 
     for dir in dirs:
         copydir(dir)
         
     for file in files:
         copyfile(file)
-
 
     style_path = os.path.join(basedir, "package", "style.css")
     with open(style_path, "r") as file:
