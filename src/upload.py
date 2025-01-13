1cdf3e2a33a53c23b43e2b9c7d938f5d5588139d
Wibo Kuipers <wibo@dtchdigitals.com>
2025-01-13 14:48:53 +0100

    Do not catch errors

--- src/upload.py
+++ src/upload.py
@@ -6,26 +6,26 @@ import subprocess
 package = "package.tar.gz"
 
 def upload_source(local_path, remote_path, host, port, user, password):
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(hostname=host, port=port, username=user, password=password)
+    # try:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=host, port=port, username=user, password=password)
 
-        subprocess.Popen(["tar", "-czf", package, "-C", local_path, "."]).wait()
-        
-        with SCPClient(ssh.get_transport()) as scp:
-            scp.put(package, os.path.join(remote_path, package))
-            stdin, stdout, stderr = ssh.exec_command(f"cd {remote_path} && tar -xzf {package}")
-            stdout.channel.recv_exit_status()  # Wait for the command to complete
-
-            print("stderr: ", stderr.read().decode())
-
-            ssh.exec_command(f"rm {os.path.join(remote_path, package)}")
-        
-        print("All files uploaded successfully.")
+    subprocess.Popen(["tar", "-czf", package, "-C", local_path, "."]).wait()
     
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    with SCPClient(ssh.get_transport()) as scp:
+        scp.put(package, os.path.join(remote_path, package))
+        stdin, stdout, stderr = ssh.exec_command(f"cd {remote_path} && tar -xzf {package}")
+        stdout.channel.recv_exit_status()  # Wait for the command to complete
+
+        print("stderr: ", stderr.read().decode())
+
+        ssh.exec_command(f"rm {os.path.join(remote_path, package)}")
     
-    finally:
-        ssh.close()
+    print("All files uploaded successfully.")
+    
+    # except Exception as e:
+    #     print(f"An error occurred: {e}")
+    
+    # finally:
+    ssh.close()
